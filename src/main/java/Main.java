597ea69eab31e82fccde2ac523b2eaae389817b8
neerajsingh <neerajmehra1997@gmail.com>
2025-07-13 23:38:17 +0530

    added client socket to listen and respond 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
@@ -11,9 +14,25 @@ public class Main {
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+         serverSocket.setReuseAddress(true);
+
+         Socket clientSocket = serverSocket.accept();
+         System.out.println("accepted new connection");
+
+         BufferedReader bufferedReader = new BufferedReader(new java.io.InputStreamReader(clientSocket.getInputStream()));
+         PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+
+         String requestMessage = bufferedReader.readLine();
+         System.out.println("Received request: " + requestMessage);
+
+         String responseMessage = requestMessage.toUpperCase();
+         responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+         printWriter.println(responseMessage);
+
+
 
        serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
