aab66990f773dfd7997356681c7f679b7b0fbf41
neerajsingh <neerajmehra1997@gmail.com>
2025-07-16 11:54:49 +0530

    url extracted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,40 +4,45 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.http.HttpClient;
 
 public class Main {
-  public static void main (String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
 
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
+            serverSocket.setReuseAddress(true);
 
-         Socket clientSocket = serverSocket.accept();
-         System.out.println("accepted new connection");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("accepted new connection");
 
-         BufferedReader bufferedReader = new BufferedReader(new java.io.InputStreamReader(clientSocket.getInputStream()));
-         PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader bufferedReader = new BufferedReader(new java.io.InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 
-         String requestMessage = bufferedReader.readLine();
-         System.out.println("Received request: " + requestMessage);
+            String requestMessage = bufferedReader.readLine();
+            String responseMessage;
+            System.out.println("Received request: " + requestMessage);
 
-         String responseMessage = requestMessage.toUpperCase();
-         responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
-         printWriter.println(responseMessage);
+            if (requestMessage != null && requestMessage.split(" ")[1].equals("/")) {
+                System.out.println("Extracted URL Path: " + requestMessage.split(" ")[1]);
+                responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+            } else {
+                responseMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+            printWriter.println(responseMessage);
 
 
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            serverSocket.accept(); // Wait for connection from client.
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
