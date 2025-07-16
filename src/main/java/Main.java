d07b9d56baefb48d1149915839c7704f114ab056
neerajsingh <neerajmehra1997@gmail.com>
2025-07-16 14:33:40 +0530

    created and sent response back to client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,19 +27,50 @@ public class Main {
             PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 
             String requestMessage = bufferedReader.readLine();
-            String responseMessage;
+            String responseMessage = null;
             System.out.println("Received request: " + requestMessage);
 
-            if (requestMessage != null && requestMessage.split(" ")[1].equals("/")) {
+
+            // Extract URL Path
+            /*if (requestMessage != null && requestMessage.split(" ")[1].startsWith("/")) {
                 System.out.println("Extracted URL Path: " + requestMessage.split(" ")[1]);
                 responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
             } else {
                 responseMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }*/
+
+            /**
+             * Respond with body: It accept the request, extract the string from url adn return in response body
+             */
+            if (requestMessage != null && requestMessage.split(" ")[1].startsWith("/")) {
+                String url = requestMessage.split(" ")[1];
+                String extractedString = url.split("/")[2];
+                System.out.println("Extracted String: " + extractedString);
+                String contentType = "Content-Type: text/plain\r\n";
+                String contentLength = "Content-Length: " + extractedString.length() + "\r\n";
+                responseMessage = "HTTP/1.1 200 OK\r\n" + contentType + contentLength + "\r\n" + extractedString;
+
+                /**
+                 * // Status line
+                 * HTTP/1.1 200 OK
+                 * \r\n                          // CRLF that marks the end of the status line
+                 *
+                 * // Headers
+                 * Content-Type: text/plain\r\n  // Header that specifies the format of the response body
+                 * Content-Length: 3\r\n         // Header that specifies the size of the response body, in bytes
+                 * \r\n                          // CRLF that marks the end of the headers
+                 *
+                 * // Response body
+                 * abc                           // The string from the request
+                 */
             }
+
+
+            System.out.println("Response message: " + responseMessage);
             printWriter.println(responseMessage);
 
 
-            serverSocket.close(); // Wait for connection from client.
+            serverSocket.accept(); // Wait for connection from client.
             System.out.println("accepted new connection");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
