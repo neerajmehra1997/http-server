31405ba879f28dfabde4324096d9827cb8915b1e
neerajsingh <neerajmehra1997@gmail.com>
2025-07-23 22:12:29 +0530

    commented header part

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -52,17 +52,17 @@ public class Main {
 
             System.out.println(requestLine);
 
-            if (requestLine.split(" ")[1].startsWith("/user")) {
+           /* if (requestLine.split(" ")[1].startsWith("/user")) {
                 String response = "HTTP/1.1 200 OK\\r\\nContent-Type: text/plain\\r\\nContent-Length: 12\\r\\n\\r\\nfoobar/1.2.3";
                 output.write(response.getBytes());
                 output.flush();
-            }
+            }*/
 
 
             /**
              * Checking for url correction or 404 not found
              */
-           /* if (requestLine != null && requestLine.split(" ")[1].equals("/")) {
+            if (requestLine != null && requestLine.split(" ")[1].equals("/")) {
                 String response = buildPlainSuccessResponse();
                 output.write(response.getBytes());
                 output.flush();
@@ -74,7 +74,7 @@ public class Main {
                 String response = buildResponse(requestLine);
                 output.write(response.getBytes());
                 output.flush();
-            }*/
+            }
 
             /*if (requestLine != null) {
                 String response = buildResponse(requestLine);
